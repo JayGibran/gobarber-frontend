06b50a6e3f0286bedd52150fa5660c754c22d5da
Jay Gibran da Silva Amaral <jaygibran@gmail.com>
2019-07-17 22:41:08 +0100

    adding forms to signup and signin and validations as well

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
 import logo from '~/assets/logo.svg';
 
+const schema = Yup.object().shape({
+  name: Yup.string().required('Name is required'),
+  email: Yup.string()
+    .email('Insert a valid email')
+    .required('E-mail is required'),
+  password: Yup.string()
+    .min(6, 'Password length should be at least 6 characters')
+    .required('Password is required'),
+});
+
 export default function SignUp() {
+  function handleSubmit(data) {}
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <form>
-        <input placeholder="Full Name" />
-        <input type="email" placeholder="Your E-mail" />
-        <input type="password" placeholder="Your Password" />
+      <Form schema={schema} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Full Name" />
+        <Input name="email" type="email" placeholder="Your E-mail" />
+        <Input name="password" type="password" placeholder="Your Password" />
         <button type="submit">Create Account</button>
         <Link to="/register">I already have login</Link>
-      </form>
+      </Form>
     </>
   );
 }

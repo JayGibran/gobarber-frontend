9023145d429b2393a93ada4a188a5bab56c45abf
Jay Gibran da Silva Amaral <jaygibran@gmail.com>
2019-07-23 23:25:12 +0100

    creating header and notification component

---
+++ src/components/Header/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Notifications from '~/components/Notifications';
+
+import logo from '~/assets/logo-purple.svg';
+import { Container, Content, Profile } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="Gobarber" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+
+        <aside>
+          <Notifications />
+          <Profile>
+            <div>
+              <strong>Jay Gibran</strong>
+              <Link to="/profile">My profile</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.png"
+              alt="Jay Gibran"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}

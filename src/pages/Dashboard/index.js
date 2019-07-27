d083cde01f271ef5787a3396a637edc313cc46fb
Jay Gibran da Silva Amaral <jaygibran@gmail.com>
2019-07-27 15:04:30 +0100

    handle dashboard dates

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
+import { format, subDays, addDays } from 'date-fns';
+import en from 'date-fns/locale/en-GB';
 import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
 import api from '~/services/api';
 
 import { Container, Time } from './styles';
 
 export default function Dashboard() {
+  const [date, setDate] = useState(new Date());
+
+  const dateFormatted = useMemo(() => format(date, 'do MMMM', { locale: en }), [
+    date,
+  ]);
+
+  function handlePreviousDay() {
+    setDate(subDays(date, 1));
+  }
+
+  function handleNextDay() {
+    setDate(addDays(date, 1));
+  }
+
   return (
     <Container>
       <header>
-        <button type="button">
+        <button onClick={handlePreviousDay} type="button">
           <MdChevronLeft size={36} color="#fff" />
         </button>
-        <strong>1th of august</strong>
-        <button type="button">
+        <strong>{dateFormatted}</strong>
+        <button onClick={handleNextDay} type="button">
           <MdChevronRight size={36} color="#fff" />
         </button>
       </header>
